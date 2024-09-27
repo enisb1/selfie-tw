cfa37ce1a4a9910377c4fe4930a20afe4ef457a6
enisb1 <b.encio.003@gmail.com>
2024-09-27 19:43:04 +0200

    add button to decide which period to show

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#dddbf1',
+        secondary: '#3c4f76',
+      },
+    },
   },
   plugins: [],
 }
