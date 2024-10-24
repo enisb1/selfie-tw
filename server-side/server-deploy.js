d6167cf4007eabd3fed7f9648080e3d330f2f45a
enisb1 <b.encio.003@gmail.com>
2024-10-24 17:48:07 +0200

    set up server

---
+++ server-side/server-deploy.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import homeRouter from './routes/homeRoutes.js';
+import notesRouter from './routes/notesRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors';
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors());
+
+//https://iamwebwiz.medium.com/how-to-fix-dirname-is-not-defined-in-es-module-scope-34d94a86694d
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // get name of the directory
+// Serve static files from the client-side/dist directory
+const client_dist_path = __dirname + '/../client-side/dist'; // build directory
+app.use(express.static(client_dist_path));  //middleware to serve static build dist for any path
+
+// routing
+app.use("/", homeRouter);
+app.use("/notes", notesRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on localhost:${PORT}`);
+})
