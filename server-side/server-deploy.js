ce8a72e52f463d35b9526b29782c50e6ee4e7a81
enisb1 <b.encio.003@gmail.com>
2024-10-26 11:32:12 +0200

    connect to mongodb cloud

--- server-side/server-deploy.js
+++ server-side/server-deploy.js
@@ -5,11 +5,24 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
 import http from "node:http";
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 8000;
 
 app.use(cors());
+
+//const mongouri = `mongodb+srv://bencio003:<db_password>@tecweb18.wgvir.mongodb.net/?retryWrites=true&w=majority&appName=Tecweb18`
+//TODO: set up env variables for mongo uri parameters
+const mongouri = `mongodb+srv://bencio003:74TG73rgjIbrzp4Q@tecweb18.wgvir.mongodb.net/tw18_db?retryWrites=true&w=majority&appName=Tecweb18`
+
+mongoose.connect(mongouri)
+    .then(() => {
+    console.log("Connection to DB successful")
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB', err);
+    });
 
 //https://iamwebwiz.medium.com/how-to-fix-dirname-is-not-defined-in-es-module-scope-34d94a86694d
 const __dirname = path.dirname(fileURLToPath(import.meta.url)); // get name of the directory
@@ -25,4 +38,6 @@ app.use("/notes", notesRouter);
 const server = http.createServer(app);
 
 // start server
-server.listen(PORT);
+server.listen(PORT, () => {
+    console.log("Server is running")
+});
