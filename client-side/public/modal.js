e6ae23ea5132a8005d8e229ead367e89c5eea01d
enisb1 <b.encio.003@gmail.com>
2024-12-23 23:18:27 +0100

    open and close modal component

--- client-side/public/modal.js
+++ client-side/public/modal.js
@@ -5,13 +5,33 @@ class Modal extends HTMLElement {
         this.shadowRoot.innerHTML =
         `
         <link rel="stylesheet" href="output.css"/>
-        <div class="fixed top-0 left-0 right-0 bottom-0 z-50 bg-black bg-opacity-20">
+        <div class="fixed top-0 left-0 right-0 bottom-0 z-50 bg-black bg-opacity-20" id="modal">
             <div class="top-1/4 absolute bg-primary text-black w-3/4 left-1/2 -translate-x-1/2 p-4
                 rounded-md shadow-sm ring-1 ring-inset ring-gray-300">
                 <slot></slot>
             </div>
         </div>
         `;
+        
+        // listener to close modal when clicking outside
+        this.modal = this.shadowRoot.getElementById('modal');
+
+        // Add event listener to close modal when clicking outside the modal content
+        this.shadowRoot.querySelector('#modal').addEventListener('click', (event) => {
+            if (event.target === event.currentTarget) {
+                this.close();
+            }
+        });
+    }
+
+    close() {
+        this.modal.classList.add('hidden');
+        this.modal.classList.remove('block');
+    }
+
+    open() {
+        this.modal.classList.remove('hidden');
+        this.modal.classList.add('block');
     }
 }
 
---
+++ client-side/public/projects.js
@@ -0,0 +1,6 @@
+function showCreateModal() {
+    const modal = document.getElementById('createProjectModal');
+    if (modal) {
+        modal.open();
+    }
+}
