b346e65718cd1758433d78635d82b418b8d79eb3
enisb1 <b.encio.003@gmail.com>
2025-03-18 20:08:09 +0100

    fix modals and add time machine frontend to projects.html

--- client-side/src/components/Modal.vue
+++ client-side/src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
     <div @click.self="$emit('close')" class="fixed top-0 left-0 right-0 bottom-0 z-50 bg-black bg-opacity-20">
         <div class="modal absolute bg-primary text-black w-3/4 left-1/2 -translate-x-1/2 p-4
-            rounded-md shadow-sm ring-1 ring-inset ring-gray-300">
+            rounded-md shadow-sm ring-1 ring-inset ring-gray-300 overflow-y-auto max-h-4/5">
             <slot></slot>
         </div>
     </div>
